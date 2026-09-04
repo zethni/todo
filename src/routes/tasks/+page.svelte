1d64f71f16657947a8b0520f368c946931784e9f
Zeth Lietzau <z@next-iteration.net>
2026-09-04 17:05:29 -0600

    got sorting of table worked out

--- src/routes/tasks/+page.svelte
+++ src/routes/tasks/+page.svelte
@@ -2,7 +2,32 @@
 	import { resolve } from '$app/paths';
 	let { data } = $props();
 
-	let tasks = data.tasks.slice(0);
+	let tasksState = $state(data.tasks);
+	let tasks = $derived(tasksState);
+
+	let taskComponents = [
+		{ title: 'Task', key: 'title' },
+		{ title: 'Effort', key: 'effort' },
+		{ title: 'Priority', key: 'priority' },
+		{ title: 'Due Date', key: 'dueDate' },
+		{ title: 'Description', key: 'description' }
+	];
+
+	/**
+	 *  @param {string} component
+	 *  @param {any} value
+	 *  @returns {any}
+	 */
+	function getComponentValue(component, value) {
+		switch (component) {
+			case 'effort':
+				return getEffortName(value);
+			case 'priority':
+				return getPriorityName(value);
+			default:
+				return value;
+		}
+	}
 
 	/** @param {number} effort @returns {string} */
 	function getEffortName(effort) {
@@ -30,49 +55,95 @@
 				return 'Unknown';
 		}
 	}
-	// function sortTasks(factor, direction) {
-	// 	console.log('sorting');
-	// 	return tasks.sort((a, b) => {
-	// 		if (direction === 'asc') {
-	// 			return a[factor] - b[factor];
-	// 		} else {
-	// 			return b[factor] - a[factor];
-	// 		}
-	// 	});
-	// }
 
-	function testSort() {
-		console.log('sorting by priority descending');
-		tasks = tasks.sort((a, b) => b.priority - a.priority);
+	/** @param {string} category
+	 *  @param {string} direction
+	 */
+	function sortTasks(category, direction = 'asc') {
+		switch (category) {
+			case 'effort':
+			case 'priority':
+				if (direction == 'asc') {
+					tasks.sort((a, b) => a[category] - b[category]);
+				} else {
+					tasks.sort((a, b) => b[category] - a[category]);
+				}
+				break;
+			default:
+				if (direction == 'asc') {
+					tasks.sort((a, b) => {
+						if (a[category] < b[category]) return -1;
+						if (a[category] > b[category]) return 1;
+						return 0;
+					});
+				} else {
+					tasks.sort((a, b) => {
+						if (a[category] > b[category]) return -1;
+						if (a[category] < b[category]) return 1;
+						return 0;
+					});
+				}
+				break;
+		}
+
+		const activeSorts = document.querySelectorAll('.sort-arrow.active');
+		activeSorts.forEach((el) => el.classList.remove('active'));
+		console.log(document.getElementById(`sort-${direction}-${category}`));
+		document.getElementById(`sort-${direction}-${category}`).classList.add('active');
 	}
 </script>
 
 <h1>Tasks</h1>
 
-{#if data.tasks.length === 0}
+{#if tasks.length === 0}
 	<p>No tasks available.</p>
 {:else}
 	<table>
 		<thead>
 			<tr>
-				<th>Task</th>
-				<th onclick={testSort}>Effort</th>
-				<th>Priority</th>
-				<th>Due Date</th>
-				<th>Description</th>
+				{#each taskComponents as component (component.key)}
+					<th>
+						{component.title}
+						<br />
+						<a
+							href="#top"
+							class={['sort-arrow', component.key == 'title' ? 'active' : '']}
+							id="sort-asc-{component.key}"
+							on:click|preventDefault={() => sortTasks(component.key, 'asc')}
+						>
+							&uarr;
+						</a>
+						<a
+							href="#top"
+							class="sort-arrow"
+							id="sort-desc-{component.key}"
+							on:click|preventDefault={() => sortTasks(component.key, 'desc')}
+						>
+							&darr;
+						</a>
+					</th>
+				{/each}
 			</tr>
 		</thead>
 		<tbody>
-			{#each tasks as { id, title, description, effort, priority, dueDate } (id)}
+			{#each tasks as task (task.id)}
 				<tr>
-					<td><a href={resolve('/tasks/' + id)}>{title}</a></td>
-
-					<td>{getEffortName(effort)}</td>
-					<td>{getPriorityName(priority)}</td>
-					<td>{dueDate}</td>
-					<td>{description}</td>
+					{#each taskComponents as component (component.key)}
+						<td>{getComponentValue(component.key, task[component.key])}</td>
+					{/each}
 				</tr>
 			{/each}
 		</tbody>
 	</table>
 {/if}
+
+<style>
+	.sort-arrow {
+		cursor: pointer;
+		font-size: 20px;
+		text-decoration: none;
+	}
+	.sort-arrow.active {
+		color: red;
+	}
+</style>
